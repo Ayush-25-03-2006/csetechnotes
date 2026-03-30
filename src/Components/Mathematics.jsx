b24c08f4859f7245bf715c405c74beb435aacb85
pythondb <ayushkumar@gmail.com>
2026-03-30 21:17:01 +0530

    Mathematics file is uploaded

--- src/Components/Mathematics.jsx
+++ src/Components/Mathematics.jsx
@@ -1,24 +1,37 @@
+import { Link } from "react-router-dom";
+import "./Mathematics.css";
+
 function Mathematics() {
     return (
-        <div className="category-grid">
+        <div className="math-container">
 
-            <Link to="/Matrix" className="card">
-                <i className="bi bi-calculator"></i>
-                <p>Matrix</p>
-            </Link>
-            <Link to="/Dc" className="card">
-                <i className="bi bi-c-square"></i>
-                <p>Differential Calculus</p>
-            </Link>
-            <Link to="/Ps" className="card">
-                <i className="bi bi-cup-hot"></i>
-                <p>Probability & Statistics</p>
-            </Link>
-            <Link to="/Discrete" className="card">
-                <i className="bi bi-diagram-3"></i>
-                <p>Discrete Mathematics</p>
-            </Link>
+            <h1 className="math-title">📘 Mathematics</h1>
+
+            <div className="category-grid">
+
+                <Link to="/Matrix" className="card">
+                    <i className="bi bi-grid-3x3-gap"></i>
+                    <p>Matrix</p>
+                </Link>
+
+                <Link to="/Dc" className="card">
+                    <i className="bi bi-graph-up"></i>
+                    <p>Differential Calculus</p>
+                </Link>
+
+                <Link to="/Ps" className="card">
+                    <i className="bi bi-bar-chart"></i>
+                    <p>Probability & Statistics</p>
+                </Link>
+
+                <Link to="/Discrete" className="card">
+                    <i className="bi bi-diagram-2"></i>
+                    <p>Discrete Mathematics</p>
+                </Link>
+
+            </div>
         </div>
-    )
+    );
 }
+
 export default Mathematics;
