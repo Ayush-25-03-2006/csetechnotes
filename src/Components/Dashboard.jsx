a30b533c03ddb9ac631ef448145f4c98d271ee80
pythondb <ayushkumar@gmail.com>
2026-03-30 20:12:56 +0530

    Dashboard Page & Navbar Changed

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,168 +1,71 @@
-import "./Dashboard.css";
-import dbms from "./Images/dbms.jpeg";
-import os from "./Images/os.jpeg";
-import oops from "./Images/oops.jpeg";
-import dsa from "./Images/dsa.jpeg";
-import network from "./Images/cn.jpeg";
-import java from "./Images/java.jpeg";
-import daa from "./Images/daa.jpeg";
-import c from "./Images/c.jpeg";
-import de from "./Images/de.jpeg";
-import calculus from "./Images/differentialcalculas.jpeg";
-import discrete from "./Images/discrete.jpeg";
-import matrix from "./Images/matrix.jpeg";
-import probability from "./Images/probability.jpeg";
-
 import { Link } from "react-router-dom";
+import imagenotes from "./Images/digital notes.webp";
+import "./Dashboard.css";
+
 function Dashboard() {
     return (
-        <div className="row row-cols-1 row-cols-md-2 g-4 ms-2 me-2">
-            <div className="col">
-                <div className="card">
-                    <img src={c} className="card-img-top" alt="C Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/C" className="btn btn-primary mb-2 btn-lg">
-                            C
-                        </Link>
-                        <h3 style={{ color: "white" }}>C Programming</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={dsa} className="card-img-top" alt="Data Structure & Algorithm" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Dsa" className="btn btn-primary mb-2 btn-lg">
-                            DSA
-                        </Link>
-                        <h3 style={{ color: "white" }}>Data Structure & Algorithm</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={daa} className="card-img-top" alt="Computer Networks" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Daa" className="btn btn-primary mb-2 btn-lg">
-                            DAA
-                        </Link>
-                        <h3 style={{ color: "white" }}>Design And Analysis Of Algorithm</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={dbms} className="card-img-top" alt="DataBase" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body ">
-                        <Link to="/Dbms" className="btn btn-primary mb-2 btn-lg">
-                            DBMS
-                        </Link>
-                        <h3 style={{ color: "white" }}>Database Management System</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={de} className="card-img-top" alt="Object Oriented Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/De" className="btn btn-primary mb-2 btn-lg">
-                            DE
-                        </Link>
-                        <h3 style={{ color: "white" }}>Digital Electronics</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={os} className="card-img-top" alt="Operating System" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Os" className="btn btn-primary mb-2 btn-lg">
-                            OS
-                        </Link>
-                        <h3 style={{ color: "white" }}>Operating System</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={java} className="card-img-top" alt="Computer Networks" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Java" className="btn btn-primary mb-2 btn-lg">
-                            JAVA
-                        </Link>
-                        <h3 style={{ color: "white" }}>Java Programming</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={network} className="card-img-top" alt="Computer Networks" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Networking" className="btn btn-primary mb-2 btn-lg">
-                            CN
-                        </Link>
-                        <h3 style={{ color: "white" }}>Computer Networks</h3>
-                    </div>
-                </div>
+        <div className="dashboard-container">
+
+            <div className="hero-section">
+                <img src={imagenotes} alt="notes" className="hero-image" />
+                <h1 className="title">Explore Top Categories</h1>
             </div>
 
-            <div className="col">
-                <div className="card">
-                    <img src={oops} className="card-img-top" alt="Object Oriented Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Oops" className="btn btn-primary mb-2 btn-lg">
-                            OOPS
-                        </Link>
-                        <h3 style={{ color: "white" }}>Object Oriented Programming System</h3>
-                    </div>
-                </div>
-            </div>
+            <div className="category-grid">
 
-            <div className="col">
-                <div className="card">
-                    <img src={matrix} className="card-img-top" alt="Object Oriented Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Matrix" className="btn btn-primary mb-2 btn-lg">
-                            Matrix
-                        </Link>
-                        <h3 style={{ color: "white" }}>Matrix & Linear Algebra</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={probability} className="card-img-top" alt="Object Oriented Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Ps" className="btn btn-primary mb-2 btn-lg">
-                            P & S
-                        </Link>
-                        <h3 style={{ color: "white" }}>Probability & Statistics</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={calculus} className="card-img-top" alt="Object Oriented Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/Dc" className="btn btn-primary mb-2 btn-lg">
-                            DC
-                        </Link>
-                        <h3 style={{ color: "white" }}>Differential Calculus</h3>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="card">
-                    <img src={discrete} className="card-img-top" alt="Object Oriented Programming" />
-                    <div style={{ backgroundColor: "#333333" }} className="card-body">
-                        <Link to="/" className="btn btn-primary mb-2 btn-lg">
-                            DM
-                        </Link>
-                        <h3 style={{ color: "white" }}>Discrete Mathematics</h3>
-                    </div>
-                </div>
+                <Link to="/Maths" className="card">
+                    <i className="bi bi-calculator"></i>
+                    <p>Mathematics</p>
+                </Link>
+
+                <Link to="/C" className="card">
+                    <i className="bi bi-c-square"></i>
+                    <p>C Programming</p>
+                </Link>
+
+                <Link to="/Java" className="card">
+                    <i className="bi bi-cup-hot"></i>
+                    <p>Java Programming</p>
+                </Link>
+
+                <Link to="/Dsa" className="card">
+                    <i className="bi bi-diagram-3"></i>
+                    <p>Data Structures</p>
+                </Link>
+
+                <Link to="/Daa" className="card">
+                    <i className="bi bi-graph-up-arrow"></i>
+                    <p>DAA</p>
+                </Link>
+
+                <Link to="/Cn" className="card">
+                    <i className="bi bi-hdd-network"></i>
+                    <p>Computer Networks</p>
+                </Link>
+
+                <Link to="/Os" className="card">
+                    <i className="bi bi-cpu"></i>
+                    <p>Operating System</p>
+                </Link>
+
+                <Link to="/Oops" className="card">
+                    <i className="bi bi-boxes"></i>
+                    <p>OOPs</p>
+                </Link>
+
+                <Link to="/De" className="card">
+                    <i className="bi bi-cpu-fill"></i>
+                    <p>Digital Electronics</p>
+                </Link>
+
+                <Link to="/Dbms" className="card">
+                    <i className="bi bi-database"></i>
+                    <p>DBMS</p>
+                </Link>
+
             </div>
         </div>
-    )
+    );
 }
-export default Dashboard
+
+export default Dashboard;
