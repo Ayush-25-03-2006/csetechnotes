7b137fe81056602612d76dc7d22b04b21de4bd58
pythondb <ayushkumar@gmail.com>
2026-07-03 00:43:28 +0530

    c_quiz file added

--- src/Components/C_Quiz.jsx
+++ src/Components/C_Quiz.jsx
@@ -1361,7 +1361,7 @@ int main(){
                     ? "correct"
                     : index === selected32
                         ? "wrong"
-                        : ""
+                        : ''
                 : ""
             }`}
             onClick={() => {
