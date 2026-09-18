bfe89a764854b7298136ed40e709643d6ba2965d
pythondb <ayushkumar@gmail.com>
2026-09-18 22:32:47 +0530

    about section updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import "./About.css";
-import about_page from "./Images/about.jpeg";
+import about_page from "./Images/about.png";
 function About() {
     return (
         <div className="contain">
